9e4a17c5b220f51b60235945af386d860b353d61
AstralFlare-owo <tuanzi_awa@qq.com>
2026-02-24 08:31:22 +0800

    Add Sub-store user service

--- modules/user/app/sub-store.nix
+++ modules/user/app/sub-store.nix
@@ -3,4 +3,24 @@
     sub-store
     sub-store-frontend
   ];
+
+  systemd.user.services.sub-store = {
+    Unit = {
+      Description = "Sub-Store";
+      After = [ "network-online.target" ];
+      Wants = [ "network-online.target" ];
+    };
+
+    Service = {
+      Type = "simple";
+      WorkingDirectory = "%h";
+      ExecStart = "${pkgs.sub-store}/bin/sub-store";
+      Restart = "on-failure";
+      RestartSec = 3;
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
