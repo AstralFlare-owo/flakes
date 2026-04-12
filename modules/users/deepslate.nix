89659fb95bb4bad7a53a76ccabdbefb4093677c5
AstralFlare-owo <tuanzi_awa@qq.com>
2026-04-12 18:52:35 +0800

    Switching to deepslate@lithosphere [3]

--- modules/users/deepslate.nix
+++ modules/users/deepslate.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   users.users.deepslate = {
     isNormalUser = true;
+    description = "深板岩酱 Deepslate";
     uid = 1000;
     extraGroups = [ "networkmanager" "wheel" "kvm" "libvirtd" "i2c" "docker" ];
     shell = pkgs.zsh;
