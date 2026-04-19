fc31b081a2e4b892125767f402051c92563bf99c
AstralFlare-owo <tuanzi_awa@qq.com>
2026-04-19 20:40:24 +0800

    Fix virtiofs

--- modules/system/virtualisation/kvm.nix
+++ modules/system/virtualisation/kvm.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }: {
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      vhostUserPackages = with pkgs; [ virtiofsd ];
+    };
+  };
   programs.virt-manager.enable = true;
 
   environment.systemPackages = with pkgs; [
