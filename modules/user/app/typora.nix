8fa704f01246953c4e26a86b11a1622e4bcc10f3
AstralFlare-owo <tuanzi_awa@qq.com>
2026-05-16 15:27:17 +0800

    Fix Typora IME problems under Wayland.

--- modules/user/app/typora.nix
+++ modules/user/app/typora.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    typora
+    (pkgs.symlinkJoin {
+      name = "typora-wrapped";
+      paths = [ typora ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/typora \
+          --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime --wayland-text-input-version=3"
+      '';
+    })
   ];
 }
