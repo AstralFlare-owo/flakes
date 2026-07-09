6be22a98a36563421c73f8acc87aa1c4a146ef68
AstralFlare-owo <46892455+AstralFlare-owo@users.noreply.github.com>
2026-07-09 23:58:11 +0800

    Fix DingTalk's file chooser, which caused crashes before

--- modules/user/app/dingtalk.nix
+++ modules/user/app/dingtalk.nix
@@ -1,5 +1,26 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    pkgs.nur.repos.yakkhini.dingtalk
+{ lib, pkgs, ... }:
+let
+  dingtalk = pkgs.nur.repos.yakkhini.dingtalk;
+  gsettingsSchemaPackages = with pkgs; [
+    gtk3
+    gsettings-desktop-schemas
+  ];
+  gsettingsSchemaPath = lib.concatMapStringsSep ":" (
+    package: "${package}/share/gsettings-schemas/${package.name}"
+  ) gsettingsSchemaPackages;
+
+  dingtalkWithGtkSchemas = pkgs.symlinkJoin {
+    name = "${dingtalk.name}-with-gtk-schemas";
+    paths = [ dingtalk ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/dingtalk \
+        --prefix XDG_DATA_DIRS : "${gsettingsSchemaPath}"
+    '';
+  };
+in
+{
+  home.packages = [
+    dingtalkWithGtkSchemas
   ];
 }
